0f484a844527eba60b568edda64a2655df9b0e69
T9RB <m9kish.play.tomsk@gmail.com>
2023-05-20 14:29:11 +0700

    add new function

--- js/authorizationFunc.js
+++ js/authorizationFunc.js
@@ -1,20 +1,2 @@
-const login = document.getElementById('loginUser').value;
-const password = document.getElementById('passwordUser').value;
 
-fetch(`http://localhost:5004/api/v1/visa/check-authorization/login=${login}/password=${password}`, {
-    method: 'GET',
-    headers: {
-        'apikey': '0e4bc10b5e334a2c9a89ede9417df633',
-        'Content-Type': 'application/json'
-    }
-})
-    .then(response => response.json())
-    .then(data => {
-        if (data.value === true){
-            location.href = "html/personalAccount.html";
-        }
-    })
-    .catch(error => {
-        // обрабатываем ошибку
-        alert("Неверный логин или пароль!")
-    });
+
--- js/getAuthForm.js
+++ js/getAuthForm.js
@@ -16,7 +16,25 @@ button_success.addEventListener('click', () => {
         alert("Логин или пароль не заполнены!");
     }
     else{
-        location.href = "html/personalAccount.html";
+        let login = document.getElementById('loginUser').value;
+        let password = document.getElementById('passwordUser').value;
+
+        fetch(`http://localhost:5004/api/v1/visa/check-authorization/login=${login}/password=${password}`, {
+            method: 'GET',
+            headers: {
+                'apikey': '0e4bc10b5e334a2c9a89ede9417df633',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.value === 'true'){
+                    location.href = "html/personalAccount.html";
+                }
+            })
+            .catch(error => {
+                alert(error);
+            });
     }
 
 });
